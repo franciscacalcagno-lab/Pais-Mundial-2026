fd57e06c32143bbab61817b68b45a2428dc12890
franciscacalcagno <franciscacalcagno@gmail.com>
2026-05-18 10:29:15 -0300

    imagen.publi

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const puntos = document.querySelectorAll('.punto');
+  const img = document.getElementById('imagenTimeline');
+
+  puntos.forEach(punto => {
+    punto.addEventListener('click', () => {
+      
+      const evento = punto.closest('.evento');
+
+      // 🔹 SI TIENE IMAGEN
+      const urlImg = evento.getAttribute('data-img');
+
+      // 🔹 SI TIENE LINK
+      const urlLink = evento.getAttribute('data-link');
+
+      if (urlLink) {
+        window.open(urlLink, "_blank"); // abre nueva pestaña
+        return;
+      }
+
+      if (urlImg) {
+        img.src = urlImg;
+        img.style.display = 'block';
+      }
+
+    });
+  });
+
+});
